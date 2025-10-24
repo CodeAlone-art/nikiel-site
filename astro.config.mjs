8b9871875f7b701aa32938b5da80fa6b6ed882ee
CodeAlone-art <tresor.200@proton.me>
2025-10-24 20:28:52 +0100

    Set Astro to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'static', // 👈 to mówi Astro, żeby budował stronę jako statyczną
+  output: 'static',
 });
